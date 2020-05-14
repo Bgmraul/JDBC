b51cf6edf0c9b0ea53b5790069b85123d2ad6c62
David Martin Garcia <dmartingarcia0@gmail.com>
2020-05-14 21:16:20 +0200

    añadiendo contenido para clase de pruebas en modelo coche

--- acciones/buscar.java
+++ acciones/buscar.java
@@ -5,10 +5,21 @@ import aplicacionJDBC.*;
 public class buscar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String nombre = JOptionPane.showInputDialog("Introduce tu nombre: ");
-		
-		Coche.consultaCoches(nombre);
-	}
+		String marca = JOptionPane.showInputDialog("Introduce tu marca: ");
+    String modelo = JOptionPane.showInputDialog("Introduce tu modelo: ");
+    String vel_max = JOptionPane.showInputDialog("Introduce tu velocidad maxima: ");
+    String aceleracion = JOptionPane.showInputDialog("Introduce tu aceleracion: ");
+    String peso = JOptionPane.showInputDialog("Introduce tu peso: ");
+    String control = JOptionPane.showInputDialog("Introduce tu control: ");
 
+		Coche.insertar(marca, modelo, vel_max, aceleracion, peso, control);
+    Coche coches[] = Coche.listar();
+    if(coches.length > 1){
+        Coche.borrar(coches[0].getId());
+		    Coche coche_actualizado = coches[coches.length - 2];
+		    coche_actualizado.setMarca("Citroen");
+		    coche_actualizado.setModelo("Berlingo");
+        coche_actualizado.actualizarBD();
+    }
+  }
 }
