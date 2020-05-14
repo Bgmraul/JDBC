88cdc95fd1a8bd108c3ba897199f6dd131f7146b
David Martín García <d.garcia@sonnen.de>
2020-05-14 20:55:11 +0200

    me gusta que se llame morfeo

--- acciones/NuevoUsuario.java
+++ acciones/NuevoUsuario.java
@@ -10,12 +10,12 @@ public class NuevoUsuario {
 		String apellidos = JOptionPane.showInputDialog("Introduce tu apellido: ");
 		String email = JOptionPane.showInputDialog("Introduce tu email: ");
 		String contrasena = JOptionPane.showInputDialog("Introduce tu contrasena: ");
-		
+
 		Usuario.insertar(nombre, apellidos, email, contrasena);
 		Usuario usuarios[]  = Usuario.listar();
 		Usuario.borrar(usuarios[0].getId());
 		Usuario usuario_actualizado = usuarios[usuarios.length - 2];
-		usuario_actualizado.setNombre("Neo");
+		usuario_actualizado.setNombre("Morfeo");
 		usuario_actualizado.setEmail("ejemplo@email.com");
 		usuario_actualizado.actualizarBD();
 	}
